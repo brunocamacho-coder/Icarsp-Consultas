274b4fe18e57eda146ee15f4f9bb198c1d2df3fd
Bruno Camacho <seuemail@gmail.com>
2026-03-11 21:49:17 -0300

    Corrige check-mp-payment para import

--- netlify/functions/check-mp-payment.js
+++ netlify/functions/check-mp-payment.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-exports.handler = async (event) => {
+export const handler = async (event) => {
   try {
     const { payment_id } = event.queryStringParameters;
     const accessToken = process.env.MP_ACCESS_TOKEN;
